826ba7c7140c7644fdfa75396db11a8aede700af
Chris Hill <chris@massdriver.cloud>
2025-04-10 10:48:43 -0700

    move date field to timestamp

--- enrich.lua
+++ enrich.lua
@@ -1,13 +1,13 @@
 index = 0
 step = os.getenv("MASSDRIVER_STEP_PATH") or "unknown"
 
-function add_index(tag, timestamp, record)
+function add_metadata(tag, timestamp, record)
     local ts = math.floor(timestamp)
     local iso8601 = os.date("!%Y-%m-%dT%H:%M:%SZ", ts)
 
     record["index"] = index
     record["step"] = step
-    record["date"] = iso8601
+    record["timestamp"] = iso8601
 
     index = index + 1
     return 1, timestamp, record
